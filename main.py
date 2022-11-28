060887ccac66df9f6fbb9d0a99aba76dfe06dd19
Paul Solomon <paul@thinkingbox.com>
2022-11-28 09:41:24 -0700

    init commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(str.islower(name))
+    print(str.isprintable(name))
+    print(str.istitle(name))
+    print(str.isascii(name))
+    print(str.isalpha(name))
+    print(f'Hi, {str.upper(name)} - {str.lower(name)} {str.capitalize(name)}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
